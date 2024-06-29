00bcda5dc6049b3f1ab1c79979e84ebb0e24e9b8
gerardjr42 <ggarciajr12@gmail.com>
2024-06-29 16:19:57 -0400

    Get /workouts
    1. Added array of workout data
    2. In app.js added route `/workouts`
    3. In workoutsControllers.js implemented route to get all workouts

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to my fitness app!")
 });
 
+//Setting up our app to use workoutControllers, which holds our routes and our workout array
+const workoutController = require("./controllers/workoutsControllers")
+//Declare app to use workoutController whenever /workouts is used in our URL
+app.use("/workouts", workoutController)
+
 module.exports = app;
 
